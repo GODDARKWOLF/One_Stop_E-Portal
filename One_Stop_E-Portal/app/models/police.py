25287d9bcf1a2cc5c5c770e8fc849b1afac9e715
DARKWOLF <jabulanichizimba1@gmail.com>
2025-11-04 15:36:48 +0200

    Completed Police Model

--- One_Stop_E-Portal/app/models/police.py
+++ One_Stop_E-Portal/app/models/police.py
@@ -15,4 +15,15 @@ class CrimeReport(BaseModel):
     type_of_crime: str
     description: str
     reported_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    reported_by: Optional[str] = None
+
+
+class CriminalRecord(BaseModel):
+    id: Optional[str] = None
+    User_id: Optional[int] = None
+    crime_committed: str = None
+    Gender: str
+    Age: int
+    sentence: Optional[str] = None
+
 
